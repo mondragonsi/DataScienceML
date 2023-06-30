eb4753e4c9f758d9dfd813f387c55a8d872a734d
Bruno Vieira Mondragon <mondragon.si@gmail.com>
2023-06-30 08:38:46 -0300

    ip address fixes on db server

--- DBAInsights/connectMSSQL.py
+++ DBAInsights/connectMSSQL.py
@@ -2,7 +2,7 @@ import pyodbc
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
 # server = 'myserver,port' # to specify an alternate port
-server = '192.168.3.116\LAB'
+server = '192.168.3.113\LAB'
 database = 'BotecoDBADB'
 username = 'python_user'
 password = '12345'
